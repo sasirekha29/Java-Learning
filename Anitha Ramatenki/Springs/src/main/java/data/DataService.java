95fcc6e2aec0922ebac63c5413261e6b919980be
ramatenki <42072152+ramatenki@users.noreply.github.com>
2022-01-06 11:24:52 +0530

    Add files via upload

---
+++ Anitha Ramatenki/Springs/src/main/java/data/DataService.java
@@ -0,0 +1,17 @@
+//Getting data
+package com.in28minutes.learnsprings.sample.enterprise.flow.data;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataService{
+public List<Integer>retrieveData(){
+	
+	return Arrays.asList(12,34,56,78,90);
+}
+	
+	
+}
